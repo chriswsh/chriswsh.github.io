1addadc20e59762cc2145257c1a497dfd04f799d
Chris Wong Sick Hong <chriswongsickhong@gmail.com>
2017-11-16 19:22:18 -0600

    Added ngSanitize, switched to non-minified js script tag

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 `use strict`;
-angular.module(`portfolioApp`, [`ngRoute`], function($httpProvider) {
+angular.module(`portfolioApp`, [`ngRoute`, `ngSanitize`], function($httpProvider) {
 /* from http://victorblog.com/2012/12/20/make-angularjs-http-service-behave-like-jquery-ajax/ */
     
     $httpProvider.defaults.headers.post[`Content-Type`] = `application/x-www-form-urlencoded;charset=utf-8`;
@@ -57,30 +57,58 @@ angular.module(`portfolioApp`).config([`navProviderProvider`, function(navProvid
 }])
 // Config Portfolio Sites - Will Pull from DB Later/Defining Data Structures/Hard Coded for Now
 .config([`portfolioProviderProvider`, function(portfolioProviderProvider) {
-    portfolioProviderProvider.configPortfolio([{
+    portfolioProviderProvider.configPortfolio([
+        {
+        title: `Courtbot`,
+        imgSmall: `/images/courtbot-small.png`,
+        imgSmallAlt: `Courtbot`,
+        imgLarge: `/images/courtbot-large.png`,
+        imgLargeAlt: `Courtbot`,
+        text: [`<p>Once people get out of jail, they're understandably reluctant to go back, even if missing a court date causes more problems. I contribute unit testing (Chai/Mocha/Sinon) and engine error logging to CourtBot, a Node.js text-message reminder system that keeps everyone on track.</p><p>I also designed the logo and created informational flyers and business cards to generate excitement and teach people how to use the service.</p>`]
+        },
+        {
+        title: `My New Author Website`,
+        imgSmall: `/images/new-book-small.png`,
+        imgSmallAlt: `My New Author Website`,
+        imgLarge: `/images/new-book-large.png`,
+        imgLargeAlt: `My New Author Website`,
+        text: [`<p>My new author website is a React/Redux front-end perched on top of a headless Drupal 8 API.</p><p>For those not interested in technical details, check out the intro animation at <a href="www.chriswsh.com">www.chriswsh.com</a>.</p>`]
+        },
+        {
         title: `The Long Shot Photography Contest`,
         imgSmall: `/images/longshot-med.png`,
         imgSmallAlt: `The Long Shot Photography Contest`,
         imgLarge: `/images/longshot-large.png`,
         imgLargeAlt: `The Long Shot Photography Contest`,
         text: [`For the Long Shot Photography Contest, I created an online home: user profiles, uploadable image galleries, and automated contest scheduling. All in an unassuming, dark frame to accentuate the art.`,
-               `I built the front end with HTML/CSS/Javascript (jQuery), and the back end with PHP/MySQL.`]},
-        
-        {title: `Gunborn: The Cygnei Saga`,
+        `I built the front end with HTML/CSS/Javascript (jQuery), and the back end with PHP/MySQL.`]
+        },
+        {
+        title: `Gunborn: The Cygnei Saga`,
         imgSmall: `/images/gunborn-med.png`,
         imgSmallAlt: `Gunborn: The Cygnei Saga`,
         imgLarge: `/images/gunborn-large.png`,
         imgLargeAlt: `Gunborn: The Cygnei Saga`,
         text: [`Gunborn was a collaboration with Idaho area web developer/sci-fi nerd John Ammon. He had a costume idea and wanted a universe to go with it.`,
-               `Photoshop design by John Ammon. Custom theming over Drupal (HTML/CSS/PHP) by Chris Wong Sick Hong.`]},
-        
-        {title: `My Author Website`,
+        `Photoshop design by John Ammon. Custom theming over Drupal (HTML/CSS/PHP) by Chris Wong Sick Hong.`]
+        },
+        {
+        title: `My Author Website`,
         imgSmall: `/images/book-med.png`,
         imgSmallAlt: `My Author Website`,
         imgLarge: `/images/book-large.png`,
         imgLargeAlt: `My Author Website`,
         text: [`I'm not just the paid shill. I'm also a client. (Does that line still work?)`,
-               `Custom theming over Drupal (HTML/CSS/PHP). Icon fonts. Dreams.`]}
+        `Custom theming over Drupal (HTML/CSS/PHP). Icon fonts. Dreams.`]
+        },
+        {
+        title: `The Willow Springs Website`,
+        imgSmall: `/images/willow-small.png`,
+        imgSmallAlt: `The Willow Springs Website`,
+        imgLarge: `/images/willow-large.png`,
+        imgLargeAlt: `The Willow Springs Website`,
+        text: [`<p>Back...way back in the early days of dinosaurs, I redesigned the website for <i>Willow Springs</i>, the literary magazine attached to the MFA Creative Writing Program at Eastern Washington University.</p><p>HTML/CSS/JavaScript.</p>`]
+        }
     ]);
 }])
 // Config Dialog Box Entries - Will Pull from DB Later/Defining Data Structures/Hard Coded for Now
